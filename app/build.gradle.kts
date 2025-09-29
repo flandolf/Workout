bd39e3002ef20a8dd15a18e0b026538ec8919de4
andy <66240551+flandolf@users.noreply.github.com>
2025-09-29 21:30:25 +1000

    feat: Update launcher icons and XML configuration for adaptive icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.flandolf.workout"
         minSdk = 34
         targetSdk = 36
-        versionName = "0.68"
-        versionCode = 68
+        versionName = "0.69"
+        versionCode = 69
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
